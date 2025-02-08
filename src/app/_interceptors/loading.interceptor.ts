ac8fb5f2d8e6e095887482cd5261cb7076596576
Silviu-Ciprian Saulea <silviu.saulea@connections.tech>
2025-02-08 02:09:31 +0200

    end of section 10

---
+++ src/app/_interceptors/loading.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { BusyService } from '../_services/busy.service';
+import { delay, finalize } from 'rxjs';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const busyservice = inject(BusyService);
+  busyservice.busy();
+
+  return next(req).pipe(
+    delay(1000),
+    finalize(() => {
+      busyservice.idle()
+    })
+  );
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent, HomeComponent]
+  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent]
 })
 
 export class AppComponent implements OnInit{
